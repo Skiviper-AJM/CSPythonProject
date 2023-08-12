0d51ad84b966dbe44f67c95e01a0e9d433cb2c09
Adam <adam.mcardlesep13@gmail.com>
2023-08-13 08:54:39 +1200

    Cleaned up enemy script

--- Enemies/Slime.gd
+++ Enemies/Slime.gd
@@ -1,118 +1,118 @@
 extends KinematicBody2D
 
-# Preloaded visual effect scene for the slime's death
-const EnemyDeath = preload("res://Effects/EnemyDeath.tscn")
+# Visual effect for slime's termination for the slime's death
+const SlimeDemiseEffect = preload("res://Effects/EnemyDeath.tscn")
 
-# Exposed variables for customizing the slime's movement characteristics from the editor
-export var ACCELERATION = 300
-export var MAX_VELOCITY = 50
-export var FRICTION = 200
-export var WANDER_FREQUENCY = 4
+# Editor customization variables for movement the slime's movement characteristics from the editor
+export var MOTION_BOOST = 300
+export var TOP_SLIME_SPEED = 50
+export var DRAG = 200
+export var ROAM_INTERVAL = 4
 
-# Enumeration for the slime's possible states
+# Enumeration for the slime's possible activity_modes
 enum {
 	STATIC,  # Slime is stationary
 	WANDER,  # Slime is wandering randomly
 	PERSUIT  # Slime is chasing the player
 }
 
-# Variables for state and movement
-var state = PERSUIT
-var velocity = Vector2.ZERO
+# Variables for activity_mode and movement
+var activity_mode = PERSUIT
+var movement_rate = Vector2.ZERO
 
-# Push vector used when the slime is hit or pushed by an external force
+# Vector for external forces on slime the slime is hit or pushed by an external force
 var push = Vector2.ZERO
 
-# References to various child nodes for different functionalities
-onready var stats = $Stats
-onready var playerDetection = $PlayerDetectionZone
-onready var sprite = $AnimatedSprite
-onready var hurtbox = $Hurtbox
-onready var softCollision = $SoftCollision
-onready var wanderController = $WanderController
-onready var animationPlayer = $AnimationPlayer
+# Node references for diverse functionalities for different functionalities
+onready var slimeAttributes = $Stats
+onready var playerLocator = $PlayerDetectionZone
+onready var slimeImage = $AnimatedSprite
+onready var damageDetector = $Hurtbox
+onready var softImpact = $SoftCollision
+onready var roamManager = $WanderController
+onready var slimeAnimation = $AnimationPlayer
 
-# Initialization function
+# Set initial parameters for the slime
 func _ready():
-	state = pick_random_state([STATIC, WANDER])
+	activity_mode = pick_random_activity_mode([STATIC, WANDER])
 
-# Main physics update function
+# Main update loop related to physics
 func _physics_process(delta): 
 	# Gradually reduce the push vector over time
 	push = push.move_toward(Vector2.ZERO, 200 * delta)
 	push = move_and_slide(push)
 	
-	# Behavior logic based on the slime's current state
-	match state:
+	# Behavior logic based on the slime's current activity_mode
+	match activity_mode:
 		STATIC:
 			# Slowly come to a stop and look for the player
-			velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+			movement_rate = movement_rate.move_toward(Vector2.ZERO, DRAG * delta)
 			find_player()
-			if wanderController.get_time_left() == 0:
+			if roamManager.get_time_left() == 0:
 				update_wander()
 		WANDER:
 			# Check for the player and wander around if not found
 			find_player()
-			if wanderController.get_time_left() == 0:
+			if roamManager.get_time_left() == 0:
 				update_wander()
-			accelerate_towards_point(wanderController.target_position, delta)
-			if global_position.distance_to(wanderController.target_position) <= 2:
-				state = STATIC
+			accelerate_towards_point(roamManager.target_position, delta)
+			if global_position.distance_to(roamManager.target_position) <= 2:
+				activity_mode = STATIC
 		PERSUIT:
-			# Chase the player if detected, else revert to STATIC state
-			var player = playerDetection.player
+			# Chase the player if detected, else revert to STATIC activity_mode
+			var player = playerLocator.player
 			if player:
 				accelerate_towards_point(player.global_position, delta)
 			else:
-				state = STATIC
+				activity_mode = STATIC
 
 	# Handle soft collisions with environment
-	if softCollision.is_colliding():
-		velocity += softCollision.get_push_vector() * delta * 400
-	velocity = move_and_slide(velocity)
+	if softImpact.is_colliding():
+		movement_rate += softImpact.get_push_vector() * delta * 400
+	movement_rate = move_and_slide(movement_rate)
 
-# Function to accelerate the slime towards a target point
+# Move slime towards a target point towards a target point
 func accelerate_towards_point(point, delta):
 	var direction = global_position.direction_to(point)
-	velocity = velocity.move_toward(direction * MAX_VELOCITY, ACCELERATION * delta)
-	sprite.flip_h = velocity.x < 0
+	movement_rate = movement_rate.move_toward(direction * TOP_SLIME_SPEED, MOTION_BOOST * delta)
+	slimeImage.flip_h = movement_rate.x < 0
 
-# Function to randomly decide between the STATIC and WANDER states for the slime
+# Choose randomly between STATIC and WANDER between the STATIC and WANDER activity_modes for the slime
 func update_wander():
-	state = pick_random_state([STATIC, WANDER])
-	wanderController.start_wander_timer(rand_range(1, 3))
+	activity_mode = pick_random_activity_mode([STATIC, WANDER])
+	roamManager.start_wander_timer(rand_range(1, 3))
 
-# Function to check if the player is within detection range
+# Determine if player is in detection range is within detection range
 func find_player():
-	if playerDetection.can_see_player():
-		state = PERSUIT
+	if playerLocator.can_see_player():
+		activity_mode = PERSUIT
 
-# Function to pick a random state from a given list
-func pick_random_state(state_list):
-	state_list.shuffle()
-	return state_list.pop_front()
+# Function to pick a random activity_mode from a given list
+func pick_random_activity_mode(activity_mode_list):
+	activity_mode_list.shuffle()
+	return activity_mode_list.pop_front()
 
-# Callback for when the slime is hit by something (e.g., player attack)
+# When slime receives damage by something (e.g., player attack)
 func _on_Hurtbox_area_entered(area):
-	stats.HP -= area.damage
+	slimeAttributes.HP -= area.damage
 	push = area.push_vector * 120
-	hurtbox.create_hit_effect()
-	hurtbox.start_invulnerability(0.3)
+	damageDetector.create_hit_effect()
+	damageDetector.start_invulnerability(0.3)
 
-# Function to instantiate and display the death effect when the slime dies
+# Display visual effect upon slime's death the death effect when the slime dies
 func create_enemy_death():
-	var enemyDeath = EnemyDeath.instance()
+	var enemyDeath = SlimeDemiseEffect.instance()
 	var world = get_tree().current_scene
 	world.add_child(enemyDeath)
 	enemyDeath.global_position = global_position
 
-# Callback for when the slime's health reaches zero
+# Handle events when slime's health depletes reaches zero
 func _on_Stats_no_HP():
 	queue_free()
 	create_enemy_death()
 
-# Callbacks for visual feedback during the slime's invulnerability period
+# Visual cues during slime's invulnerability phase during the slime's invulnerability period
 func _on_Hurtbox_invulnerability_started():
-	animationPlayer.play("Start")
+	slimeAnimation.play("Start")
 func _on_Hurtbox_invulnerability_ended():
-	animationPlayer.play("Stop")
+	slimeAnimation.play("Stop")
